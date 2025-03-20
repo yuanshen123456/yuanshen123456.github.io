59367d895cddc8ea58832b5df209ee9c72db77d5
yuanwq <yuanwq@123>
2025-03-20 17:52:34 +0800

    部署：更新编译后的文件到根目录

--- assets/AboutView-Ds9h9AJ1-DcW-3ccx.js
+++
@@ -1,1 +0,0 @@
-import{f as a,P as n,a as s,n as o}from"./index-Cl9iM7Nw.js";const r={},t={class:"about"},u=a("h1",null,"This is an about page",-1),e=[u];function i(c,f){return s(),o("div",t,e)}const l=n(r,[["render",i]]);export{l as default};
